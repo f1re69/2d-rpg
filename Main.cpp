2708bb61d4cce35e70630fef691657ba3e0f8afb
Khelil <kbezzouh@gmail.com>
2019-01-05 17:19:28 +0100

    First commit by Khelil

---
+++ Main.cpp
@@ -0,0 +1,133 @@
+#include <SFML/Graphics.hpp>
+#include <windows.h>
+#include <string>
+#include <iostream>
+using namespace std;
+void gestion_clavier();
+
+#pragma region variables
+sf::RenderWindow window;
+sf::Texture perso;
+sf::Sprite spritePerso;
+sf::Vector2i anim(1, 10);
+sf::Clock clockTime;
+enum Direction { Down = 10, Up = 8, Left = 9, Right = 11 };	// emplacement pixel top left du sprite de départ multiplié par BLOCKSIZE
+int speed = 4;
+const int BLOCKSIZE = 64;
+const int BLOCKSIZE_WIDTH_MOVMENT = 9;
+const int MAX_SPEED = 12;
+const int MIN_SPEED = 5;
+bool updateFps = true;	// si false, animation stoppée 
+#pragma endregion variables
+
+int main() {
+
+	// Création de la fenêtre (800x600 avec un titre "Tuto SFML"
+	window.create (sf::VideoMode(800, 600), "Tuto SFML");
+	// 60 fps
+	window.setFramerateLimit(60);
+
+	// Chargement des sprites
+	if (!perso.loadFromFile("sprites.png")) {
+		cout << "impossible de charger le sprite" << endl;
+	}
+	perso.setSmooth(true);
+	spritePerso.setTexture(perso);
+	
+	
+
+	while (window.isOpen()) {
+
+		if (updateFps) {	
+		
+			// Une image toutes les 50ms
+			if (clockTime.getElapsedTime().asMilliseconds() >= 50) {
+				anim.x++;
+				//cout << clockTime.getElapsedTime().asMilliseconds() << endl << "anim.x : " << anim.x << endl;
+				if (anim.x * BLOCKSIZE >= BLOCKSIZE * BLOCKSIZE_WIDTH_MOVMENT) {
+					anim.x = 0;
+				}
+				clockTime.restart();
+			}
+		}
+
+		else {
+			// Repositionnement de l'image à la position 0
+			while (anim.x != 0) {
+				if (clockTime.getElapsedTime().asMilliseconds() >= 30) {
+					anim.x++;
+					if (anim.x * BLOCKSIZE >= BLOCKSIZE * BLOCKSIZE_WIDTH_MOVMENT) {
+						anim.x = 0;
+					}
+					clockTime.restart();
+				}
+			}
+		}
+
+		spritePerso.setTextureRect(sf::IntRect(anim.x * BLOCKSIZE, anim.y * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE));
+		window.draw(spritePerso);
+		window.display();
+		
+		sf::Event event;
+
+		// Test des évenements
+		while (window.pollEvent(event)) {
+
+			// Fermeture de la fenêtre lors du clique sur la croix rouge
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		gestion_clavier();
+
+		//if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+
+		window.clear();
+
+	}
+
+
+	return 0;
+}
+
+// Gestion des event clavier : Déplacement up,down,left,right ...
+void gestion_clavier() {
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		updateFps = true;
+		anim.y = Up;
+		spritePerso.move(0, -speed);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		updateFps = true;
+		anim.y = Down;
+		spritePerso.move(0, speed);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		updateFps = true;
+		anim.y = Left;
+		spritePerso.move(-speed, 0);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		updateFps = true;
+		anim.y = Right;
+		spritePerso.move(speed, 0);
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		updateFps = false;
+		if (speed + 1 < MAX_SPEED) {
+			speed += 1;
+		}
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+		updateFps = false;
+		if (speed - 1 > MIN_SPEED) {
+			speed -= 1;
+		}
+	}
+	// !updateFps = Le personnage arrête de se déplacer 
+	else {
+		updateFps = false;
+	}
+}
